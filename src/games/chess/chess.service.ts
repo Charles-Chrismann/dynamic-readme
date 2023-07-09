a9a714f64001982ff8a043d619eccb9885107107
Charles-Chrismann <charles.chrismann@free.fr>
2023-07-09 19:02:07 +0200

    re-add moves

--- src/games/chess/chess.service.ts
+++ src/games/chess/chess.service.ts
@@ -8,6 +8,33 @@ import { utils } from './classes/utils';
 export class ChessService {
     chess: Chess
 
+    pieces = {
+        Pawn: {
+            white: "♙",
+            black: "♟",
+        },
+        King: {
+            white: "♔",
+            black: "♚",
+        },
+        Queen: {
+            white: "♕",
+            black: "♛",
+        },
+        Rook: {
+            white: "♖",
+            black: "♜",
+        },
+        Knight: {
+            white: "♘",
+            black: "♞",
+        },
+        Bishop: {
+            white: "♗",
+            black: "♝",
+        },
+    }
+
     constructor() {
         this.chess = new Chess()
     }
@@ -65,6 +92,15 @@ export class ChessService {
         let str = `<h3 align="center">A classic Chess</h3>`
         this.renderBoardImage()
         str += `<p align="center"><img width="256" src="${process.env.EC2_PROTOCOL}://${process.env.EC2_SUB_DOMAIN}.${process.env.EC2_DOMAIN}/board.png" /></p>`
+
+        utils.getAllPieces(this.chess.board).filter(piece => piece.color === this.chess.playerTurn).forEach(piece => {
+            if(piece.legalMoves.length === 0) return
+            let pieceStr = "<p>" + this.pieces[piece.type][piece.color] + utils.coordsToBoardCoards({x: piece.x, y: piece.y}) + ": "
+            piece.legalMoves.forEach(move => {
+                pieceStr += `<a href="${process.env.EC2_PROTOCOL}://${process.env.EC2_SUB_DOMAIN}.${process.env.EC2_DOMAIN}/chess/move?x1=${piece.x}&y1=${piece.y}&x2=${move.x}&y2=${move.y}">${utils.coordsToBoardCoards({x: move.x, y: move.y})}</a> `
+            })
+            str += pieceStr + "</p>"
+        })
 
         str += `<h3 align="center"><a href="${process.env.EC2_PROTOCOL}://${process.env.EC2_SUB_DOMAIN}.${process.env.EC2_DOMAIN}/chess/new">Reset Game</a></h3>`
         return str
