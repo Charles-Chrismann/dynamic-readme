562038a2378084ff6d6423b956b4c351876c6501
Charles <charles.chrismann@free.fr>
2024-08-29 09:54:08 +0200

    fixe size

--- src/games/wordle/wordle.service.ts
+++ src/games/wordle/wordle.service.ts
@@ -122,7 +122,7 @@ export class WordleService implements IReadmeModule {
     str += `  </tbody>\n</table>\n`
 
     str += `<table align="center">\n  <thead>\n    <tr>\n      <th colspan="4">Scoreboard</th>\n    </tr>\n    <tr>\n      <th>Rank</th>\n      <th colspan="2">Player</th>\n      <th>Wins</th>\n    </tr>\n  </thead>\n  <tbody>\n`
-    str += scoreBoard.users.map((user, index) => `    <tr>\n      <td align="center">${index + 1}</td>\n      <td align="center">\n        <a href="https://github.com/${user.username}">\n          <img src="https://avatars.githubusercontent.com/u/${user.userId}?size=32">\n        </a>\n      </td>\n      <td>\n        <a href="https://github.com/${user.username}">@${user.username}</a>\n      </td>\n      <td align="center">${user.guesses}</td>\n    </tr>\n`).join('')
+    str += scoreBoard.users.map((user, index) => `    <tr>\n      <td align="center">${index + 1}</td>\n      <td align="center">\n        <a href="https://github.com/${user.username}">\n          <img src="https://avatars.githubusercontent.com/u/${user.userId}?size=32" width="40" height="40"/>\n        </a>\n      </td>\n      <td>\n        <a href="https://github.com/${user.username}">@${user.username}</a>\n      </td>\n      <td align="center">${user.guesses}</td>\n    </tr>\n`).join('')
     str += `  </tbody>\n</table>\n\n`
 
     str += `<hr>\n\n`
