a62684e2328020633afe272d83604032b6dfdafd
Charles-Chrismann <charles.chrismann@free.fr>
2026-04-24 15:03:37 +0200

    fix with

--- src/modules/statics/skills.module.ts
+++ src/modules/statics/skills.module.ts
@@ -18,7 +18,7 @@ export class SkillsStaticModule extends AbstractStaticModule<Data, Options> {
     if(skills.learning) {
       md += `<h3 align="left">Currently learning:\n`;
       md += skills.learning.list.map((skill) => {
-        return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" width="40" height="40"/>\n  </a>\n`;
+        return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" height="40"/>\n  </a>\n`;
       }).join('');
       md += `</h3>\n`;
     }
@@ -26,28 +26,28 @@ export class SkillsStaticModule extends AbstractStaticModule<Data, Options> {
     // Front
     md += `<h3>Front-end technologies</h3>\n<p align="left">\n`;
     md += skills.front.list.map((skill) => {
-      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" width="40" height="40"/>\n  </a>\n`;
+      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" height="40"/>\n  </a>\n`;
     }).join('');
     md += `</p>\n`;
 
     // Back
     md += `<h3>Back-end technologies</h3>\n<p align="left">\n`;
     md += skills.back.list.map((skill) => {
-      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" width="40" height="40"/>\n  </a>\n`;
+      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" height="40"/>\n  </a>\n`;
     }).join('');
     md += `</p>\n`;
 
     // Notions
     md += `<h3>Other technologies where I have notions</h3>\n<p align="left">\n`;
     md += skills.notions.list.map((skill) => {
-      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" width="40" height="40"/>\n  </a>\n`;
+      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" height="40"/>\n  </a>\n`;
     }).join('');
     md += `</p>\n`;
 
     // Tools
     md += `<h3>Tools</h3>\n<p align="left">\n`;
     md += skills.tools.list.map((skill) => {
-      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" width="40" height="40"/>\n  </a>\n`;
+      return `  <a href="${skill.url}" target="_blank" rel="noreferrer">\n    <img src="${skill.src}" alt="${skill.alt}" height="40"/>\n  </a>\n`;
     }).join('');
     md += `</p>\n`;
 
