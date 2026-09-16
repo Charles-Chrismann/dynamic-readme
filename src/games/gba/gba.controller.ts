8365c1541b81b8ca4d98aaf1881dbca881ef6cc9
Charles-Chrismann <charles.chrismann@free.fr>
2026-09-16 19:56:16 +0200

    fix

--- src/games/gba/gba.controller.ts
+++ src/games/gba/gba.controller.ts
@@ -48,7 +48,7 @@ export class GbaController {
   }
 
   @Get('/:id/gif')
-  @Header('Cache-Control', 'no-store, no-cache, must-revalidate')
+  @Header('Cache-Control', 'max-age=0, no-store, no-cache, must-revalidate')
   @Header('Content-Type', 'image/gif')
   gif(
     @Param('id') id: string
